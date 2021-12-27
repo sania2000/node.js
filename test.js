2c490c6df8dc1afe9e33635e4bbcfd48de5ab915
Sania Safari <saniasafariii@gmail.com>
2021-12-27 23:57:46 +0330

    df

---
+++ test.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+const PORT = 2500;
+const path = require('path')
+
+let id = 100;
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, id + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({storage:storage})
+
+app.post("/upload", upload.single('image'), (req, res) => {
+    if(req.file){        
+        res.send("Your image has been uploaded under the name: " + req.file.filename);
+        id = id + 1;
+    }
+    else {
+        return 'error'
+    }
+})
+
+app.get('/images/:id', function(req, res) {
+    res.sendFile(__dirname + "/images/" + req.params.id)   
+});
+
+app.listen(PORT, () => {
+    console.log('Listening at ' + PORT );
+});
